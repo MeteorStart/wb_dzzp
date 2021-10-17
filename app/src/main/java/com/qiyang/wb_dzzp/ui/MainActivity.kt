eba9ce1b9ec1a43202f8be23a20ba13095da91ef
lx802315@163.com <lx802315@163.com>
2021-10-17 12:47:23 +0800

    增加UI显示

--- app/src/main/java/com/qiyang/wb_dzzp/ui/MainActivity.kt
+++ app/src/main/java/com/qiyang/wb_dzzp/ui/MainActivity.kt
@@ -4,12 +4,31 @@ import android.os.Bundle
 import com.qiyang.wb_dzzp.R
 import com.qiyang.wb_dzzp.base.BaseActivity
 import com.qiyang.wb_dzzp.databinding.ActivityMainBinding
+import com.qiyang.wb_dzzp.utils.RecycleViewUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    var dataList = ArrayList<String>()
+    val mainAdapter = MainAdapter(R.layout.item_bus, dataList)
 
     override fun getLayoutId(): Int = R.layout.activity_main
 
     override fun initActivity(savedInstanceState: Bundle?) {
         NavigationBarStatusBar(this, true)
+        initRecy()
+        initData()
+    }
+
+    private fun initRecy() {
+        recy_main.layoutManager = RecycleViewUtils.getVerticalLayoutManagerNoDecoration(this, recy_main)
+        recy_main.adapter = mainAdapter
+    }
+
+    private fun initData() {
+        for (i in 0..10) {
+            dataList.add(i.toString())
+        }
+        mainAdapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/qiyang/wb_dzzp/ui/MainAdapter.kt
@@ -0,0 +1,18 @@
+package com.qiyang.wb_dzzp.ui
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+
+/**
+ * @author: X_Meteor
+ * @description: 类描述
+ * @version: V_1.0.0
+ * @date: 10/15/21 2:26 PM
+ * @company:
+ * @email: lx802315@163.com
+ */
+class MainAdapter(layoutResId: Int, data: MutableList<String>?) : BaseQuickAdapter<String, BaseViewHolder>(layoutResId, data) {
+    override fun convert(helper: BaseViewHolder?, item: String?) {
+
+    }
+}
