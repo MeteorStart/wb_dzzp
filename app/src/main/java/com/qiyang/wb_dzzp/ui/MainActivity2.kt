ba0f54760707712304f0469c2145474f2cd09ee6
X_Meteor <lx802315@163.com>
2022-04-06 21:30:38 +0800

    增加页面

---
+++ app/src/main/java/com/qiyang/wb_dzzp/ui/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.qiyang.wb_dzzp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qiyang.wb_dzzp.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
